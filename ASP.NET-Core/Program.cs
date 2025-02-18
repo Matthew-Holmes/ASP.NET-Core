1ee4e85ebd0d20173e6857266674d02f39a16980
Matthew <matthew.holmes111@gmail.com>
2025-02-18 15:08:40 +0000

    middleware and another endpoint

--- ASP.NET-Core/Program.cs
+++ ASP.NET-Core/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.HttpLogging;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Matthew Holmes", "Matthew"));
 
 app.Run();
+
+public record Person(string fullname, string preferredname);
