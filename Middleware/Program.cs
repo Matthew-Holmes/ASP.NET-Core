3a3c8007735c59962342bef41f8da1ea0509135c
Matthew <matthew.holmes111@gmail.com>
2025-02-19 17:29:29 +0000

    environment specific exception handling middleware

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -2,9 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //app.UseWelcomePage();
-app.UseDeveloperExceptionPage();
+//app.UseDeveloperExceptionPage(); // automatic if in dev environment
 app.UseStaticFiles();
 app.UseRouting();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/async", async () =>
@@ -12,5 +17,8 @@ app.MapGet("/async", async () =>
     await Task.Delay(100);
     throw new Exception("my custom async exception");
 });
+
+app.MapGet("/error", () => "Sorry, an error occurred");
+
 
 app.Run();
