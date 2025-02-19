6b33471df938fc8afd702b72cbc4a5e70e19b95b
Matthew <matthew.holmes111@gmail.com>
2025-02-19 17:19:35 +0000

    demo of developer exception page

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -7,6 +7,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/async", async () =>
+{
+    await Task.Delay(100);
+    throw new Exception("my custom async exception");
+});
 
 app.Run();
