95d60e049b92e889fe020b53af6a9aebe0be1b26
Matthew <matthew.holmes111@gmail.com>
2025-02-25 17:20:03 +0000

    LinkGenerator demo

--- EndpointRouting/Program.cs
+++ EndpointRouting/Program.cs
@@ -10,4 +10,16 @@ app.MapGet("/", () => "Hello World!"); // minimal API endpoint
 app.MapHealthChecks("/healthz");       // register a health-check endpoint 
 app.MapRazorPages();                   // register all razor pages as endpoints
 
+// link generator
+app.MapGet("/product/{name}", (string name) => $"the product is {name}")
+        .WithName("product"); // metadata
+
+app.MapGet("/links", (LinkGenerator links) =>
+{
+    /* creates a lik using the route name */
+    string link = links.GetPathByName("product",
+        new { name = "big-widget" });
+    return $"view the product at {link}";
+});
+
 app.Run();
