16e1cfca1c2aef81098374fe3b8adb6842ad127c
Matthew <matthew.holmes111@gmail.com>
2025-02-27 10:50:16 +0000

    custom binding with BindAsync

--- ModelBinding/Program.cs
+++ ModelBinding/Program.cs
@@ -12,6 +12,7 @@ app.MapGet("/paged-products/{id}/paged",
     => $"received id {id}, page {page}, pageSize {pageSize}");
 
 app.MapGet("/product/{id}", (ProductId id) => $"recieved {id}");
+app.MapPost("/size", (SizeDetails size) => $"recieved {size}");
 
 // warning - never run any files uploaded!
 app.MapPost("/upload", (IFormFile file) => $"recieved file of size {file.Length}")
@@ -34,6 +35,26 @@ readonly record struct ProductId(int id)
         result = default;
         return false;
     }
-
 }
+
+// custom binding
+public record SizeDetails(double height, double width)
+{
+    public static async ValueTask<SizeDetails?> BindAsync(
+        HttpContext context)
+    {
+        using var sr = new StreamReader(context.Request.Body);
+
+        string? line1 = await sr.ReadLineAsync(context.RequestAborted);
+        if (line1 is null) { return null; }
+
+        string? line2 = await sr.ReadLineAsync(context.RequestAborted);
+        if (line2 is null) { return null; }
+
+        return double.TryParse(line1, out double height)
+            && double.TryParse(line2, out double width)
+            ? new SizeDetails(height, width) : null;
+    }
+}
+
 
