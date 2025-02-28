60a573c92d968e2b17c25ed89e5e99a95d05cf6b
Matthew <matthew.holmes111@gmail.com>
2025-02-28 10:44:45 +0000

    demo of registering custom services

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,5 +1,12 @@
 
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddScoped<NetworkClient>();
+builder.Services.AddSingleton<MessageFactory>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -18,3 +25,25 @@ public interface IEmailSender
 {
     public void SendEmail(string username);
 }
+
+public class EmailSender : IEmailSender
+{
+    public void SendEmail(string username)
+    {
+        // sketch of what a true implementation might look like
+        NetworkClient  nc = new NetworkClient();
+        MessageFactory mf = new MessageFactory();
+
+
+        Debug.WriteLine($"email sent to {username} by EmailSender");
+    }
+}
+
+public class NetworkClient
+{
+
+}
+
+public class MessageFactory
+{ }
+
