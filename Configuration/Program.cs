a7c19d609e46bbb952c9d3f70ccb68dbabce199c
Matthew <matthew.holmes111@gmail.com>
2025-03-04 11:16:57 +0000

    only load config from appsettings.json

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.Sources.Clear();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
