d6bbbafef3480eeac48445e6c2af2454b66e8762
Matthew <matthew.holmes111@gmail.com>
2025-03-05 10:39:08 +0000

    strongly typed config demo

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // later additions can overwrite ealier
@@ -5,11 +7,11 @@ builder.Configuration.Sources.Clear();
 builder.Configuration.AddJsonFile("appsettings.json", optional: true);
 builder.Configuration.AddJsonFile("sharedSettings.json", optional: true);
 builder.Configuration.AddEnvironmentVariables();
+//builder.Configuration.AddEnvironmentVariables("SomePrefix"); // only variables like SomePrefix__MyVar
 
 var zoomlevel = builder.Configuration["MapSettings:DefaultZoomLevel"];
 var lat1 = builder.Configuration["MapSettings:DefaultLocation:Latitude"];
 var lat2 = builder.Configuration.GetSection("MapSettings")["DefaultLocation:Latitude"];
-
 
 
 var app = builder.Build();
@@ -18,5 +20,21 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/config", () => app.Configuration.AsEnumerable());
 // can also access via dependency injection
 app.MapGet("/config/di", (IConfiguration config) => config.AsEnumerable());
+app.MapGet("/display-settings",
+    (IOptions<AppDisplaySettings> options) =>
+{
+    AppDisplaySettings settings = options.Value;
+    string title = settings.Title;
+    bool showCopyright = settings.ShowCopyright;
+
+    return new { title, showCopyright };
+});
 
 app.Run();
+
+
+public class AppDisplaySettings
+{
+    public string Title { get; set; } = string.Empty;
+    public bool ShowCopyright { get; set; } = false;
+}
