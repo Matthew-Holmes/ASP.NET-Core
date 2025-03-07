9c8e8bdac710b11fcde73e2ba1841b18fa72ca54
Matthew <matthew.holmes111@gmail.com>
2025-03-07 10:10:49 +0000

    used client created by NSwag

--- OpenAPI_NSwag/Program.cs
+++ OpenAPI_NSwag/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fruit;
+
+
+FruitClient fc = new FruitClient(
+    "http://localhost:5000",
+    new HttpClient());
+
+Fruit.Fruit created = await fc.FruitPOSTAsync("123",
+    new Fruit.Fruit { Name = "Banana", Stock = 100 });
+Console.WriteLine($"created {created.Name}");
+
+Fruit.Fruit fetched = await fc.FruitGETAsync("123");
+Console.WriteLine($"fetched {fetched.Name}");
+
