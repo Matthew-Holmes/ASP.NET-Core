0e9bfe97e58bbe16c74e3f2155ad512a5df81c6f
Matthew <matthew.holmes111@gmail.com>
2025-03-08 14:57:22 +0000

    sample data

--- EF_Core/Program.cs
+++ EF_Core/Program.cs
@@ -1,5 +1,59 @@
+using System.Collections.Concurrent;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+#region demo data
+Ingredient _egg   = new Ingredient(0, "egg",               "eggs");
+Ingredient _flour = new Ingredient(1, "plain white flour", "grams");
+Ingredient _milk  = new Ingredient(2, "whole milk",        "millilitres");
+
+Recipe _pancakes = new Recipe(
+    0,
+    "pancakes",
+    new Dictionary<Ingredient, decimal>
+    {
+        { _egg, 2.0m },
+        { _flour, 500.0m },
+        { _milk, 100.0m }
+    },
+    new List<String> 
+    { 
+        "crack the eggs into a bowl",
+        "stir all ingredients into the bowl with the eggs",
+        "heat a pan",
+        "oil the pan",
+        "fry one pancake at a time for roughly 2 minutes, turning once",
+        "serve immediately"
+    });
+
+Recipe _boiledEgg = new Recipe(
+    1,
+    "hard boiled egg",
+    new Dictionary<Ingredient, decimal>
+    {
+        { _egg, 2.0m },
+    },
+    new List<string>
+    {
+        "boil the egg in a pan of water for 10 minutes",
+        "peel and serve"
+    });
+#endregion
+
+ConcurrentBag<Ingredient> _ingredients = new ConcurrentBag<Ingredient>
+{
+    _egg,
+    _flour,
+    _milk,
+};
+
+ConcurrentBag<Recipe> _recipes = new ConcurrentBag<Recipe>
+{
+    _boiledEgg,
+    _pancakes,
+};
+
 
 app.MapGet("/", () => "Welcome to recipe world!");
 
@@ -9,7 +63,7 @@ public record Ingredient(int Id, String Name, String Unit);
 public record Recipe(
     int Id,
     String Name,
-    List<Dictionary<Ingredient, decimal>> Ingredients,
+    Dictionary<Ingredient, decimal> Ingredients,
     List<String> steps);
 
 
