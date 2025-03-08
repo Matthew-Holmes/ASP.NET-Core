82f449156e8180378563ea81551c1145a83fa4b2
Matthew <matthew.holmes111@gmail.com>
2025-03-08 14:47:23 +0000

    data model

--- EF_Core/Program.cs
+++ EF_Core/Program.cs
@@ -4,3 +4,14 @@ var app = builder.Build();
 app.MapGet("/", () => "Welcome to recipe world!");
 
 app.Run();
+
+public record Ingredient(int Id, String Name, String Unit);
+public record Recipe(
+    int Id,
+    String Name,
+    List<Dictionary<Ingredient, decimal>> Ingredients,
+    List<String> steps);
+
+
+
+
