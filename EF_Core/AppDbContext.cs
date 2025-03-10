08cc0f019b75af16518ecae2080a41a5ce48a593
Matthew <matthew.holmes111@gmail.com>
2025-03-10 11:12:11 +0000

    added AppDbContext

---
+++ EF_Core/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        // list of root entities
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
