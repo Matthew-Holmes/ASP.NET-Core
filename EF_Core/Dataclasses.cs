1ffa917331b3446b8994f48e8f82039cec58b2a4
Matthew <matthew.holmes111@gmail.com>
2025-03-10 11:06:05 +0000

    added sample entity dataclasses

---
+++ EF_Core/Dataclasses.cs
@@ -0,0 +1,19 @@
+﻿namespace EF_Core
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public required string Name { get; set; }
+        public TimeSpan TimeToCook { get; set; }
+        public bool IsDeleted { get; set; }
+        public required string Method { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int IngredientID { get; set; }
+        public int RecipeID { get; set; }
+        public required string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public required string Unit { get; set; }
+}
